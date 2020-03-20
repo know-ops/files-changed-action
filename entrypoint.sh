c283a78f6149fbddce7414c30ecb847d68abed68
Graham Burgess <stormmore@gmail.com>
2020-03-19 20:19:50 -0700

    [debug] add debug log line

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,7 @@
 FILES=$1
 REGEX=$2
 
+echo "::debug file=entrypoint.sh,line=6::cd ${GITHUB_WORKSPACE}"
 cd ${GITHUB_WORKSPACE}
 if [ "${REGEX}" != "" ]; then
   FILES=$(echo -n $(git diff --name-only --diff-filter=${FILES} HEAD~1 HEAD | grep -e "${REGEX}") | tr ' ' ',')
