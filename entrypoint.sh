755c87831d6d45abf457d872b4701be2de253634
Graham Burgess <stormmore@gmail.com>
2020-03-19 20:27:27 -0700

    [debug] additional debug logging

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env sh
 
 FILES=$1
+echo "::debug file=entrypoint.sh,line=4::${FILES}"
 REGEX=$2
+echo "::debug file=entrypoint.sh,line=6::${REGEX}"
 
-echo "::debug file=entrypoint.sh,line=6::cd ${GITHUB_WORKSPACE}"
+echo "::debug file=entrypoint.sh,line=8::cd ${GITHUB_WORKSPACE}"
 cd ${GITHUB_WORKSPACE}
 if [ "${REGEX}" != "" ]; then
   FILES=$(echo -n $(git diff --name-only --diff-filter=${FILES} HEAD~1 HEAD | grep -e "${REGEX}") | tr ' ' ',')
